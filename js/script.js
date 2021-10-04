449b30039a8abe5e714573d099d7a8c8ea05b1c6
Pepetka <vlad.kznetsov@mail.ru>
2021-10-04 13:31:34 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -58,3 +58,34 @@ function choice3() {
   document.getElementById('gray2').style.display = "inline-block";
   document.getElementById('gray3').style.display = "none";
 }
+function burger() {
+  var menu = document.getElementById('menu-box');
+  var burger = document.getElementById('burger-menu');
+  var checkbox = document.getElementById('checkbox');
+  if (checkbox.checked) {
+    menu.style.display = 'block';
+    burger.classList.toggle("change");
+  }
+  else {
+    menu.style.display = 'none';
+    burger.classList.toggle("change");
+  }
+}
+const mediaQuery1 = window.matchMedia('(min-width: 768px)')
+function block(e) {
+  let a = document.getElementById('menu-box');
+  let b = document.getElementById('checkbox');
+  if (e.matches && a.style.display == 'none') {
+    a.style.display = 'block';
+  }
+  else {
+    if (b.checked) {
+      a.style.display = 'block';
+    }
+    else {
+      a.style.display = 'none';
+    }
+  }
+}
+mediaQuery1.addListener(block);
+block(mediaQuery1);
